d5995dca2e3c928b1cf6119f2426c161bf4017d2
Norbert Feron <me@n6f.fr>
2016-06-17 18:14:27 +0200

    Get Post by Type improve with user id

--- routes/post/post_getter.py
+++ routes/post/post_getter.py
@@ -1,8 +1,9 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from neo4j.v1 import ResultError
 from connector import neo4j
 from routes.utils import addargs, makeResponse
 
+parser = reqparse.RequestParser()
 
 class GetPost(Resource):
     def get(self, post_id):
@@ -79,12 +80,31 @@ class GetPostsByAuthor(Resource):
 
 class GetPostType(Resource):
     def get(self):
-        # todo add optional userId  to count <-[]-(u:user {uid: })
-        req = "MATCH (n:post_type)<-[r:TYPE_IS]-(p:post)  RETURN n, count(r) AS nb_posts"
+        params = []
+        parser.add_argument('uid', action='append')
+        args = parser.parse_args()
+
+        if args['uid']:
+            req = "MATCH (n:post_type)<-[r:TYPE_IS]-(p:post) "
+            for user in args['uid']:
+                req += "OPTIONAL MATCH (n)<-[r%s:TYPE_IS]-(p:post)<-[]-(u%s:user {uid: %s}) " % (user, user, user)
+            req += "RETURN n, count(r) AS nb_posts"
+            for user in args['uid']:
+                req += ", count(r%s) AS u%s_posts" % (user, user)
+        else:
+            req = "MATCH (n:post_type)<-[r:TYPE_IS]-(p:post)  RETURN n, count(r) AS nb_posts"
         result = neo4j.query_neo4j(req)
         labels = []
         data = [[]]
+        if args['uid']:
+            for user in args['uid']:
+                data.append([])
         for record in result:
             labels.append(record['n'].properties['name'])
             data[0].append(record['nb_posts'])
+            if args['uid']:
+                count = 1;
+                for user in args['uid']:
+                    data[count].append(record['u%s_posts' % user])
+                    count += 1
         return makeResponse([{'labels': labels, 'data': data}], 200)
