a17c70981feea22527a714fd8e184fe09e198312
nferon <norbert.feron@etu.u-bordeaux.fr>
2017-04-26 11:21:15 +0200

    doi and layout action type

--- graphtulip/manage.py
+++ graphtulip/manage.py
@@ -36,11 +36,10 @@ def save(trace_id):
 
 
 def addStep(data):
-    # load graph
+    # Load graph
     graph = root.getSubGraph("graph")
     trace = root.getSubGraph("trace" + data['room'])
-    # add a Step
-    newNode = trace.addNode()
+
     label = trace.getLocalStringProperty("name")
     color = trace.getLocalColorProperty("viewColor")
     layout = trace.getLocalStringProperty("layout")
@@ -48,47 +47,60 @@ def addStep(data):
     selection = trace.getLocalStringProperty("selection")
     type = trace.getLocalStringProperty("type")
 
+    # Add a Step
+    newNode = trace.addNode()
     label[newNode] = "step " + str(newNode.id)
     layout[newNode] = data['layout']
+    type[newNode] = data['type']
     size[newNode] = data['size']
     selection[newNode] = str(list(data['selection'].values()))
-    type[newNode] = data['type']
 
-    # trace the edge and label it
-    if data['actual'] != None:
+    if data['type'] == 'doi':
+        # Trace the edge and label it
+        if data['actual'] != None:
+            for n in trace.nodes():
+                if str(n.id) == str(data['actual']):
+                    edge = trace.addEdge(n, newNode)
+                    edge_label = ""
+                    if layout[n] != layout[newNode]:
+                        edge_label += layout[newNode]
+                    if selection[n] != selection[newNode]:
+                        edge_label += " selection"
+                    if size[n] != size[newNode]:
+                        size = size[newNode] - size[n]
+                        if size > 0:
+                            edge_label += " size +" + str(size)
+                        else:
+                            edge_label += " size " + str(size)
+                    if type[n] != type[newNode]:
+                        edge_label += " " + type[newNode]
+                    label[edge] = edge_label
+
+        computeDoi = ComputeDoi(graph)
+        result = computeDoi.create(list(data['selection'].values()), data['size'], str(newNode.id))
+        view_selection = result.getLocalBooleanProperty('viewSelection')
+        view_selection.setAllEdgeValue(False)
+        view_selection.setAllNodeValue(False)
+        for n in result.nodes():
+            if str(n.id) in list(data['selection'].values()):
+                view_selection[n] = True
+    else:
         for n in trace.nodes():
-            # color[n] = tlp.Color(255, 95, 95)
             if str(n.id) == str(data['actual']):
                 edge = trace.addEdge(n, newNode)
-                edge_label = ""
-                if layout[n] != layout[newNode]:
-                    edge_label += layout[newNode]
-                if selection[n] != selection[newNode]:
-                    edge_label += " selection"
-                if size[n] != size[newNode]:
-                    size = size[newNode] - size[n]
-                    if size > 0:
-                        edge_label += " size +" + str(size)
-                    else:
-                        edge_label += " size " + str(size)
-                if type[n] != type[newNode]:
-                    edge_label += " " + type[newNode]
-                label[edge] = edge_label
+        label[edge] = layout[newNode]
 
-    # color[newNode] = tlp.Color(20, 20, 255)
+        actual = graph.getSubGraph(str(data['actual']))
+        result = actual.addCloneSubGraph(str(newNode.id), True, True)
+
+    # Apply layout on the trace
     params = tlp.getDefaultPluginParameters('Tree Leaf', trace)
     params['orientation'] = "right to left"
     trace.applyLayoutAlgorithm("Tree Leaf", trace.getLocalLayoutProperty("viewLayout"), params)
-    # apply DOI
-    computeDoi = ComputeDoi(graph)
-    result = computeDoi.create(list(data['selection'].values()), data['size'], str(newNode.id))
-    view_selection = result.getLocalBooleanProperty('viewSelection')
-    view_selection.setAllEdgeValue(False)
-    view_selection.setAllNodeValue(False)
-    for n in result.nodes():
-        if str(n.id) in list(data['selection'].values()):
-            view_selection[n] = True
+
+    # Apply layout on the graph
+    # tlp.saveGraph(root, "data/tlp/root.tlpb")
     params = tlp.getDefaultPluginParameters(data['layout'], result)
-    # tlp.saveGraph(result, "data/tlp/save.tlpb")
     result.applyLayoutAlgorithm(data['layout'], result.getLocalLayoutProperty("viewLayout"), params)
+
     return trace, newNode.id
